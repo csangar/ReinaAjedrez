85bca9a39a619edf9f8d85a975b948782f9071f6
Usuario <Usuario@DESKTOP-ILM66FC>
2022-11-14 14:09:45 +0100

    Enumerado color completo

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,6 +1,15 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
-	BLANCO,
-	NEGRO
+	BLANCO("Blanco"),
+	NEGRO("Negro");
+	
+	private String cadenaAMostrar;
+
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	public String toString() {
+		return cadenaAMostrar;
+	}
 }
