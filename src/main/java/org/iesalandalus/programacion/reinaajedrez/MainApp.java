6d5b0a7f68ca63ed97b8bb636cf96d9d8fb46eea
Usuario <Usuario@DESKTOP-ILM66FC>
2022-11-17 14:05:51 +0100

    Metodo main empezado

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,25 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
 	
 	public static void main(String[] args) {
+		
+	}
+	private void ejecutarOpcion(int opcion) {
+		
+	}
+	private void crearReinaDefecto() {
+		
+	}
+	private void ReinaColor(){
+		
+	}
+	private void mover() {
+		
+	}
+	private void mostrarReina() {
 		
 	}
 }
